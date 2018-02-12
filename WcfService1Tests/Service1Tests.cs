d306c62ca99d67d109fb0436e8ad4f83d3dbc707
Lasse Andersen <LasseAndersen@users.noreply.github.com>
2018-02-12 11:21:35 +0100

    Add project files.

---
+++ WcfService1Tests/Service1Tests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfService1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService1.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void GetMyEmailTest()
+        {
+            //Arrange
+
+            var service1 = new Service1();
+
+            //Act
+
+            string email = service1.GetMyEmail();
+
+            //Assert
+
+            Assert.AreEqual("Lasses Email", email);
+        }
+
+        [TestMethod()]
+        public void GetMyNameTest()
+        {
+            var service2 = new Service1();
+
+            string navn = service2.GetMyName();
+            
+            Assert.AreEqual("Lasse", navn);
+        }
+    }
+}
